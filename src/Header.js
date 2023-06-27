f2a940aa1397350d19b8a58a6d584a8b0b6ba304
Mohd Aqib <anmolali5811@gmail.com>
2023-06-27 17:03:48 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,51 @@
+import {Link} from "react-router-dom";
+import {useContext, useEffect, useState} from "react";
+import {UserContext} from "./UserContext";
+
+export default function Header() {
+  const {setUserInfo,userInfo} = useContext(UserContext);
+  useEffect(() => {
+    try{
+      fetch('http://localhost:8000/profile', {
+        credentials: 'include',
+      }).then(response => {
+        response.json().then(userInfo => {
+          setUserInfo(userInfo);
+        });
+      });
+    }catch(err){
+      console.log(err);
+    }
+    
+  }, []);
+
+  function logout() {
+    fetch('http://localhost:8000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
+  return (
+    <header>
+      <Link to="/" className="logo">MyBlog</Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <a onClick={logout}>Logout ({username})</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
